2b9bd638f5fddf96db3cce2fb0b6d9a5353f74ba
Estaban <e.boin@esbocreations.com>
2013-04-10 11:02:57 +0200

    Added Sylex vendor pack (fat edition)

---
+++ vendor/symfony/finder/Symfony/Component/Finder/Exception/ExceptionInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Symfony\Component\Finder\Exception;
+
+/**
+ * @author Jean-François Simon <contact@jfsimon.fr>
+ */
+interface ExceptionInterface {
+  /**
+   * @return \Symfony\Component\Finder\Adapter\AdapterInterface
+   */
+  public function getAdapter();
+}
